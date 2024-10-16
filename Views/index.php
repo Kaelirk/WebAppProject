0cdedf1750b99b0a021044b6cea5af4f377124ab
Kaelirk <stephenferns92@gmail.com>
2024-10-17 00:21:25 +0200

    16/10/2024 - OOPphp

--- Views/index.php
+++ Views/index.php
@@ -1,10 +1,12 @@
+
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <link href="styles.css" rel="stylesheet">
 
 <nav class="navbar bg-body-tertiary bg-dark" data-bs-theme="dark">
   <div class="container-fluid">
   <a class="navbar-brand" href="#">
-      <img src="dogtena.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      <img src="/images/dogtena.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
       DogTena
     </a>
   </div>
---
+++ includes/class-autolaod.inc.php
@@ -0,0 +1,11 @@
+<?php 
+
+spl_autoload_register('myAutoLoader');
+
+function myAutoLoade ($className) {
+    $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+
+    $path = 'classes/';
+    $extension = '.class.php';
+    require_once $path . $className . $extension;
+}
