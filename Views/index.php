7e43cb3e7397751cc0cda554cd0507abfa0a5f82
Kaelirk <stephenferns92@gmail.com>
2024-11-06 16:55:16 +0100

    substituted mysqli for PDO

--- Views/index.php
+++ Views/index.php
@@ -1,8 +1,12 @@
 <?php
   include '../includes/class-autoload.inc.php';
   ?>
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <link href="styles.css" rel="stylesheet">
 
 <nav class="navbar bg-body-tertiary bg-dark" data-bs-theme="dark">
@@ -13,23 +17,17 @@
     </a>
   </div>
 </nav>
+  <title>Dogtena</title>
+</head>
+<body>  
+  <?php
+    $testObj= new Test();
+    $testObj->getUsers();
 
-<?php
+  ?>
+</body>
+</html>
 
-$connect = mysqli_connect("db","lamp_docker","password","lamp_docker");
 
-$query = 'select * from test_table';
-$result = mysqli_query($connect, $query);
 
-echo '<h1>Temporary table</h1>';
-
-while($record = mysqli_fetch_assoc($result)){
-    echo '<h2>'.$record['id'].'</h2>';
-    echo '<p>'.$record['name'].'</p>';
-    echo '<p>'.$record['second name'].'</p>';
-    echo 'Posted:' .$record['date'];
-    echo '<hr>';
-}
-
-echo 'does this update live?';
 
--- includes/class-autoload.inc.php
+++ includes/class-autoload.inc.php
@@ -5,7 +5,7 @@ spl_autoload_register('myAutoLoader');
 function myAutoLoader ($className) {
     $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 
-    $path = 'classes/';
+    $path = '../classes/';
     $extension = '.class.php';
     require_once $path . $className . $extension;
 }
